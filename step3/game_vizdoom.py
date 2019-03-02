4f44a506fbacf8ed592ae84f7f25431920304abe
Mario Siegenthaler <mario.siegenthaler@linkyard.ch>
2019-03-02 23:59:31 +0100

    feat: add deadly corridor scenario

--- step3/game_vizdoom.py
+++ step3/game_vizdoom.py
@@ -1,31 +1,23 @@
+from abc import ABC
+
 import numpy as np
 from vizdoom import DoomGame
 
 from drl.deepq.game import Game, Action, Frames, Experience
 
 
-class VizdoomBasicGame(Game):
-  """Vizdoom Basic => One room with a single non-moving opponent"""
-
-  actions = [Action("left", [1, 0, 0], 0),
-             Action("right", [0, 1, 0], 1),
-             Action("fire", [0, 0, 1], 2)]
-
-  def __init__(self, x: int, y: int, t: int):
-    self.game = self._setup_game()
+class VizdoomGame(Game, ABC):
+  def __init__(self, scenario: str, x: int, y: int, t: int):
+    self.game = self._setup_game(scenario)
     self.frames = Frames(x, y, t)
     self._new_episode()
     self.frame_shape = self.game.get_state().screen_buffer.shape
 
-  @property
-  def name(self) -> str:
-    return "vizdoom-basic"
-
   @staticmethod
-  def _setup_game():
+  def _setup_game(scenario: str):
     game = DoomGame()
-    game.load_config("data/vizdoom/simpler_basic.cfg")
-    game.set_doom_scenario_path("data/vizdoom/simpler_basic.wad")
+    game.load_config('data/vizdoom/%s.cfg' % scenario)
+    game.set_doom_scenario_path('data/vizdoom/%s.wad' % scenario)
     game.set_window_visible(False)
     game.init()
     return game
@@ -65,3 +57,45 @@ class VizdoomBasicGame(Game):
       frame = self.game.get_state().screen_buffer
       self.frames.add_frame(frame)
       return reward, False
+
+
+class VizdoomBasicGame(VizdoomGame):
+  """Vizdoom Basic => One room with a single non-moving opponent"""
+
+  actions = [Action("left", [1, 0, 0], 0),
+             Action("right", [0, 1, 0], 1),
+             Action("fire", [0, 0, 1], 2)]
+
+  def __init__(self, x: int, y: int, t: int):
+    super().__init__('simpler_basic', x, y, t)
+
+  @property
+  def name(self) -> str:
+    return "vizdoom-basic"
+
+
+class VizdoomCorridorGame(VizdoomGame):
+  """Vizdoom Corridor => One corridor with oponents"""
+
+  actions = [Action("move_left",
+                    [1, 0, 0, 0, 0, 0, 0], 0),
+             Action("move_right",
+                    [0, 1, 0, 0, 0, 0, 0], 1),
+             Action("fire",
+                    [0, 0, 1, 0, 0, 0, 0], 2),
+             Action("move_forward",
+                    [0, 0, 0, 1, 0, 0, 0], 3),
+             Action("move_backward",
+                    [0, 0, 0, 0, 1, 0, 0], 4),
+             Action("turn_left",
+                    [0, 0, 0, 0, 0, 1, 0], 5),
+             Action("turn_right",
+                    [0, 0, 0, 0, 0, 0, 1], 6),
+             ]
+
+  def __init__(self, x: int, y: int, t: int):
+    super().__init__('deadly_corridor', x, y, t)
+
+  @property
+  def name(self) -> str:
+    return "vizdoom-corridor"
