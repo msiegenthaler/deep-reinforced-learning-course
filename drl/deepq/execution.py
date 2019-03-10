fix: no backprop on gamma

drl/deepq/execution.py
@@ -19,7 +19,9 @@ def best_action(device, policy_net: nn.Module, state: Tensor) -> int:
     # t.max(1) will return largest column value of each row.
     # second column on max result is index of where max element was
     # found, so we pick action with the larger expected reward.
-    return policy_net(s).max(1)[1].view(1, 1).item()
+    qs = policy_net(s)
+    index = qs.max(1)[1].view(1, 1).item()
+    return index
 
 
 def run_episode(model: ExecutionModel) -> (float, int, Dict[str, int]):

drl/deepq/learn.py
@@ -16,7 +16,7 @@ def get_target_action_values(model: LearningModel, gamma: float, exps: [Experien
   next_state_values = torch.zeros(len(exps), device=model.device)
   next_state_values[non_final_mask] = model.target_net(next_states).max(1)[0].detach()
   lengths = tensor([e.state_difference_in_steps for e in exps]).to(model.device)
-  gammas = torch.pow(tensor(gamma).to(model.device), lengths.float())
+  gammas = torch.pow(tensor(gamma).to(model.device), lengths.float()).detach()
 
   rewards = tensor([e.reward for e in exps], device=model.device)
   target_action_values = (next_state_values * gammas) + rewards